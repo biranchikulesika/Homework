9a241f1acdc8fe4339808519a161f031ed205bad
biranchikulesika <biranchikulesika@gmail.com>
2025-03-12 21:15:29 +0530

    Add Python program to remove duplicates from an array

---
+++ py1_remove_duplicate.py
@@ -0,0 +1,21 @@
+#Remove duplicates from an array
+
+def removeDuplicate(array):
+    i = 0
+
+    array.sort()
+
+    for j in range(1,len(array)):
+        if array[j] != array[i]:
+            i += 1
+            array[i] = nums[j]
+    return i + 1
+
+print("Remove duplicate elements from an array.\n\n")
+
+nums = list(map(int, input("Enter the elements separated by space: ").split()))
+
+unique_elements = removeDuplicate(nums)
+
+print(unique_elements)
+print(nums[:unique_elements])
