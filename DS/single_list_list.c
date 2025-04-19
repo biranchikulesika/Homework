b79dc66841c4c956f819fbb60cf7251350ac127c
biranchikulesika <biranchikulesika@gmail.com>
2025-04-20 02:24:57 +0530

    Add implementation of singly linked list with basic operations

--- DS/single_list_list.c
+++ DS/single_list_list.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a singly linked list node
 typedef struct Node
 {
     int data;
     struct Node *next;
 } SLL;
 
-// Function to create a new node
 SLL *CreateNode(int value)
 {
     SLL *newNode = (SLL *)malloc(sizeof(SLL));
     if (!newNode)
     {
+
         printf("Error: Memory allocation failed in CreateNode.\n");
         return NULL;
     }
@@ -22,51 +21,55 @@ SLL *CreateNode(int value)
     return newNode;
 }
 
-// Function to insert a node at the end of the list
-SLL *InsertAtEnd(SLL *head, int value)
+void InsertAtEnd(SLL **head, int value)
 {
     SLL *newNode = CreateNode(value);
-    if (newNode == NULL)
+    if (!newNode)
     {
-        return head; // Return the unchanged list if memory allocation fails
+
+        return;
     }
 
-    if (head == NULL)
+    if (!(*head))
     {
-        return newNode; // If the list is empty, the new node becomes the head
+
+        *head = newNode;
+        return;
     }
 
-    SLL *temp = head;
+    SLL *temp = *head;
     while (temp->next != NULL)
     {
         temp = temp->next;
     }
+
     temp->next = newNode;
-    return head;
 }
 
-SLL *InsertAtBeginning(SLL *head, int value)
+void InsertAtBeginning(SLL **head, int value)
 {
     SLL *newNode = CreateNode(value);
-    if (newNode == NULL)
+    if (!newNode)
     {
-        return head; // Return the unchanged list if memory allocation fails
+
+        return;
     }
 
-    newNode->next = head; // Point the new node to the current head
-    return newNode;       // The new node becomes the new head
+    newNode->next = *head;
+    *head = newNode;
 }
 
-// Function to print the linked list
 void PrintList(SLL *head)
 {
     if (head == NULL)
     {
+
         printf("List is empty.\n");
         return;
     }
 
     SLL *temp = head;
+
     while (temp != NULL)
     {
         printf("%d -> ", temp->data);
@@ -75,19 +78,18 @@ void PrintList(SLL *head)
     printf("NULL\n");
 }
 
-// Function to free all nodes in the linked list
 void FreeList(SLL *head)
 {
     SLL *temp;
     while (head != NULL)
     {
+
         temp = head->next;
         free(head);
         head = temp;
     }
 }
 
-// Main function
 int main()
 {
     int n, data;
@@ -95,6 +97,7 @@ int main()
     printf("Enter the number of elements: ");
     if (scanf("%d", &n) != 1 || n < 0)
     {
+
         printf("Invalid input. Exiting.\n");
         return 1;
     }
@@ -106,11 +109,12 @@ int main()
         printf("Enter the data: ");
         if (scanf("%d", &data) != 1)
         {
+
             printf("Invalid input. Exiting.\n");
-            FreeList(list1); // Free any allocated memory before exiting
+            FreeList(list1);
             return 1;
         }
-        list1 = InsertAtEnd(list1, data);
+        InsertAtEnd(&list1, data);
     }
 
     PrintList(list1);
