c46caa9e5670b6a3ad003e568e1f11821c4c48c4
biranchikulesika <biranchikulesika@gmail.com>
2025-07-04 00:49:04 +0530

    Add Tower of Hanoi implementation and external stylesheet demo

---
+++ APP/TowerOfHanoi.py
@@ -0,0 +1,11 @@
+def hanoi(n, start, end):
+    if n == 1:
+        pm(start, end)
+    else:
+        other = 6 - (start + end)
+        hanoi(n-1, start, other)
+        pm(start, end)
+        hanoi(n-1, other, end)
+
+def pm(start, end):
+    print(start, '->', end)
